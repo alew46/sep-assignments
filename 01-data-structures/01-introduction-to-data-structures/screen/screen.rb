c6453beaa4ae15270930ad38c724ed502f69bbd8
Alex Lewis <alew46@gmail.com>
2018-12-30 17:04:07 -0500

    all screen tests pass

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,14 +6,20 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = Array.new(width) {
+      Array.new(height, nil)
+    }
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
-
+    self.matrix[x][y] = pixel
   end
 
   def at(x, y)
+    self.matrix[x][y]
   end
 
   private
